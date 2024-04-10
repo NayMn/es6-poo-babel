a7833240fdb12c561b35dcda632a5b05373f708f
najira <najira_daniela@hotmail.com>
2024-04-09 21:18:39 -0400

    clientes nuevos creados

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 import Cliente from "./main.js";
 import Impuestos from "./impuestos.js";
+
+let primerImpuesto = new Impuestos(100, 20);
+
+
+let primerCliente = new Cliente("josefa", new Impuestos(600, 30));
+let segundoCliente = new Cliente("pamela", primerImpuesto);
+let tercerCliente = new Cliente("fernanda", new Impuestos(700, 40));
+
+primerCliente.calcularImpuesto();
+tercerCliente.calcularImpuesto();
